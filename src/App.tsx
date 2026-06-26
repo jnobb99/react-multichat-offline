b33d13fc451c3fab769b92124834809bb556ce63
bonieky <bonieky@gmail.com>
2026-06-26 09:53:56 -0300

    fase1

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-export default function App() {
-  return (
-    <div>
+import Chat from './components/Chat'
 
-    </div>
-  )
+export default function App() {
+  return <Chat />
 }
---
+++ src/components/Chat.tsx
@@ -0,0 +1,9 @@
+export default function Chat() {
+  return (
+    <div className="min-h-dvh bg-stone-200">
+      <div className="mx-auto flex h-dvh max-w-2xl flex-col">
+        {/* histórico e input serão adicionados nas próximas fases */}
+      </div>
+    </div>
+  )
+}
